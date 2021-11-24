13d29eadb9da2bc17c921661ec02da4ec9549b46
mandriianova <mandriianova@neuro.net>
2021-11-24 23:16:35 +0300

    Add 3_longest_substring_without_repeating_characters

---
+++ 3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = current = 0
+        i = 0
+        seen = set()
+        while i < len(s):
+            if s[i] not in seen:
+                seen.add(s[i])
+                current += 1
+            else:
+                j = i - current
+                to_remove = set()
+                while s[j] != s[i]:
+                    if s[j] == s[i]:
+                        break
+                    to_remove.add(s[j])
+                    j += 1
+                    current -= 1
+                seen -= to_remove
+            longest = max(longest, current)
+            i += 1
+        return longest
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    assert solution.lengthOfLongestSubstring(s="abcabcbb") == 3
+    assert solution.lengthOfLongestSubstring(s="bbbbb") == 1
+    assert solution.lengthOfLongestSubstring(s="pwwkew") == 3
+    assert solution.lengthOfLongestSubstring(s="") == 0
+    assert solution.lengthOfLongestSubstring(s="aababcabcdabcde") == 5
+    assert solution.lengthOfLongestSubstring(s="abcdabccbcbdfa") == 5
+    assert solution.lengthOfLongestSubstring(s=" ") == 1
+    assert solution.lengthOfLongestSubstring(s="au") == 2
