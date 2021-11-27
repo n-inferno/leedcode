cb0db26d682b700f980f56e012061c79cd5de908
mandriianova <mandriianova@neuro.net>
2021-11-27 22:01:05 +0300

    Add another solution 238_product_of_array_except_self

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -31,6 +31,19 @@ class Solution:
 
         return answer
 
+    # O(n) without division
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = [1]
+        for i in range(1, len(nums)):
+            product.append(product[i - 1] * nums[i - 1])
+
+        p = 1
+        for i in range(len(nums) - 1, -1, -1):
+            product[i] *= p
+            p *= nums[i]
+
+        return product
+
 
 if __name__ == '__main__':
     solution = Solution()
