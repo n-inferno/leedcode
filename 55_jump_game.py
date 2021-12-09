eb59e60986ac1496896bfc0049e8f8d463509eca
maria <marya.andrijanowa@yandex.ru>
2021-12-09 15:41:47 +0300

    Add 55_jump_game recursive

--- 55_jump_game.py
+++ 55_jump_game.py
@@ -28,13 +28,24 @@ class Solution:
 
         return True if goal == 0 else False
 
+    # greedy recursive
+    def canJump(self, nums: List[int]) -> bool:
+        sys.setrecursionlimit(11000)
+
+        def helper(goal):
+            if goal == 0:
+                return True
+            for i in range(goal - 1, -1, -1):
+                if nums[i] + i >= goal:
+                    return helper(i)
+            else:
+                return False
+
+        return helper(len(nums) - 1)
+
 
 if __name__ == '__main__':
     solution = Solution()
     assert solution.canJump(nums=[2, 3, 1, 1, 4]) is True
     assert solution.canJump(nums=[3, 2, 1, 0, 4]) is False
     assert solution.canJump(nums=[1]) is True
-    assert solution.canJump(
-        nums=[8, 2, 4, 4, 4, 9, 5, 2, 5, 8, 8, 0, 8, 6, 9, 1, 1, 6, 3, 5, 1, 2, 6, 6, 0, 4, 8, 6, 0, 3, 2, 8, 7, 6, 5,
-              1, 7, 0, 3, 4, 8, 3, 5, 9, 0, 4, 0, 1, 0, 5, 9, 2, 0, 7, 0, 2, 1, 0, 8, 2, 5, 1, 2, 3, 9, 7, 4, 7, 0, 0,
-              1, 8, 5, 6, 7, 5, 1, 9, 9, 3, 5, 0, 7, 5]) is True
