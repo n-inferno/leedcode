3a8755ce1378caa3b0642e82d1b68cff2a5f9d7c
mandriianova <and.mo@skbkontur.ru>
2022-04-22 10:40:25 +0300

    Change 355_design_twitter

--- 355_design_twitter.py
+++ 355_design_twitter.py
@@ -6,23 +6,23 @@ from typing import List
 class Twitter:
 
     def __init__(self):
-        self.user_twits = defaultdict(list)
         self.follow_list = defaultdict(set)
-        self.global_order_identifier = 0
+        self.twit_storage = []
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.global_order_identifier += 1
-        self.user_twits[userId].append((self.global_order_identifier, tweetId))
+        self.twit_storage.append((userId, tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
         user_follows = self.follow_list[userId]
         user_follows.add(userId)
         news = []
-        for followee in user_follows:
-            news.extend(self.user_twits[followee])
+        i = len(self.twit_storage) - 1
+        while len(news) < 10 and i >= 0:
+            if self.twit_storage[i][0] in user_follows:
+                news.append(self.twit_storage[i][1])
+            i -= 1
 
-        news.sort(key=lambda twit: twit[0], reverse=True)
-        return [n[1] for n in news[:10]]
+        return news
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.follow_list[followerId].add(followeeId)
