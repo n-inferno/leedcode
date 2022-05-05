f8af9254efbd076c9d14f542bb135efc17832ada
mandriianova <and.mo@skbkontur.ru>
2022-05-05 16:45:13 +0300

    Add new solution for 733_flood_fill

--- 733_flood_fill.py
+++ 733_flood_fill.py
@@ -3,25 +3,27 @@ from typing import List
 
 
 class Solution:
-    def helper(self, image, i, j, current_color, new_color):
-        if not 0 <= i < len(image) or not 0 <= j < len(image[i]) or image[i][j] != current_color:
-            return
-        image[i][j] = new_color
-        self.helper(image, i - 1, j, current_color, new_color)
-        self.helper(image, i, j + 1, current_color, new_color)
-        self.helper(image, i + 1, j, current_color, new_color)
-        self.helper(image, i, j - 1, current_color, new_color)
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        color = image[sr][sc]
-        if color == newColor:
+        old_color = image[sr][sc]
+
+        def helper(x, y):
+            if x not in range(len(image)) or y not in range(len(image[0])) or image[x][y] != old_color:
+                return
+
+            image[x][y] = newColor
+
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                helper(x + dx, y + dy)
+
+        if newColor == old_color:
             return image
-        self.helper(image, sr, sc, color, newColor)
+        helper(sr, sc)
         return image
 
 
 if __name__ == '__main__':
     solution = Solution()
+    assert solution.floodFill(image=[[0, 0, 0], [1, 0, 0]], sr=1, sc=0, newColor=2) == [[0, 0, 0], [2, 0, 0]]
     assert solution.floodFill(image=[[1, 1, 1], [1, 1, 0], [1, 0, 1]], sr=1, sc=1, newColor=2) == [[2, 2, 2], [2, 2, 0],
                                                                                                    [2, 0, 1]]
     assert solution.floodFill(image=[[0, 0, 0], [0, 0, 0]], sr=0, sc=0, newColor=2) == [[2, 2, 2], [2, 2, 2]]
