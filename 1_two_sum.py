00c2e1b1b852a3f5b15ad6beba7ea89ec24e65ca
mandriianova <and.mo@skbkontur.ru>
2022-05-23 23:42:08 +0300

    Optimize 1_two_sum

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -3,12 +3,12 @@ from typing import List
 
 
 class Solution:
-    # O(n^2) solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            seen[num] = i
 
 
 if __name__ == '__main__':
