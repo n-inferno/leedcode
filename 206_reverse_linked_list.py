e3a8db7498d642b70cae43748eef6c81ac95e8ff
mandriianova <and.mo@skbkontur.ru>
2022-05-25 11:26:38 +0300

    Change 206_reverse_linked_list

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -6,31 +6,26 @@ from helpers import ListNode, to_list, to_linked_list
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        result = track = None
+        new_head = None
 
-        def helper(node):
-            nonlocal result, track
+        def helper(head):
+            nonlocal new_head
 
-            if not node:
-                return
+            if not head or not head.next:
+                new_head = head
+                return head
 
-            helper(node.next)
-            if not result:
-                result = track = ListNode()
-            track.val = node.val
-
-            if node != head:
-                track.next = ListNode()
-
-            track = track.next
+            node = helper(head.next)
+            node.next = head
+            head.next = None
+            return head
 
         helper(head)
-        return result
+        return new_head
 
 
 if __name__ == '__main__':
     solution = Solution()
-    assert to_list(solution.reverseList(head=to_linked_list([1, 1, 2, 3, 4, 4]))) == [4, 4, 3, 2, 1, 1]
-    assert to_list(solution.reverseList(head=to_linked_list([]))) == []
-    assert to_list(solution.reverseList(head=to_linked_list([1]))) == [1]
     assert to_list(solution.reverseList(head=to_linked_list([1, 2]))) == [2, 1]
+    assert to_list(solution.reverseList(head=to_linked_list([1, 1, 2, 3, 4, 4]))) == [4, 4, 3, 2, 1, 1]
+    assert to_list(solution.reverseList(head=to_linked_list([1]))) == [1]
