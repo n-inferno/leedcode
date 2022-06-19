fe49e72a6a0cdc6b0104864d1c3a1389e4465d36
mandriianova <and.mo@skbkontur.ru>
2022-06-19 14:43:15 +0300

    Add 203_remove_linked_list_elements

---
+++ 203_remove_linked_list_elements.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/remove-linked-list-elements/
+from typing import Optional
+
+from helpers import ListNode
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        while head and head.val == val:
+            head = head.next
+
+        node = head
+        while node and node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+            else:
+                node = node.next
+
+        return head
